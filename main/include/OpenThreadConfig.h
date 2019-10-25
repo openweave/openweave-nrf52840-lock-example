882b0e2cdb21c0d7a8853b73ae051da85850ca00
Pierre Delisle <pierred@google.com>
2019-10-25 11:47:44 -0700

    Fix for issue 39: Thread configuration adjustments

--- main/include/OpenThreadConfig.h
+++ main/include/OpenThreadConfig.h
@@ -30,8 +30,16 @@
 // openweave/src/adaptations/device-layer/nRF5/Logging.cpp).
 #define OPENTHREAD_CONFIG_LOG_OUTPUT OPENTHREAD_CONFIG_LOG_OUTPUT_APP
 
-// Turn on a moderate level of logging in OpenThread
+// Turn on a moderate level of logging in OpenThread.
+// Set to OT_LOG_LEVEL_INFO or OT_LOG_LEVEL_DEBG for more verbose output if experiencing
+// issues with Thread.
 #define OPENTHREAD_CONFIG_LOG_LEVEL OT_LOG_LEVEL_NOTE
+
+// When operating in a less than ideal RF environment, having a more forgiving configuration
+// of OpenThread makes thread a great deal more reliable.
+#define OPENTHREAD_CONFIG_MAC_MAX_FRAME_RETRIES_DIRECT 15 // default is 3
+#define OPENTHREAD_CONFIG_MAC_MAX_FRAME_RETRIES_INDIRECT 1 // default is 0
+#define OPENTHREAD_CONFIG_MAC_MAX_TX_ATTEMPTS_INDIRECT_POLLS 16 // default is 4
 
 // Use the Nordic-supplied default platform configuration for remainder
 // of OpenThread config options.
